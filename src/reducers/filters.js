38f119e8475636e31b2b5ee16925b84cc31e7aa9
Dan Paddock <danpaddock@dans-air.lan>
2020-08-21 15:35:36 -0400

    filter by date

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -3,8 +3,8 @@ import moment from 'moment';
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
   };
   
   export default (state = filtersReducerDefaultState, action) => {
