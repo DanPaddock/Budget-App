db24c80af305fcac487f8d681bd06c450bffca0b
Dan Paddock <danpaddock@dans-air.lan>
2020-08-25 10:32:23 -0400

    firebase integration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,18 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCjzGI_6WPXBqwjtPw1bu-9P7WH_UzTxOI",
-    authDomain: "budget-app-56ef8.firebaseapp.com",
-    databaseURL: "https://budget-app-56ef8.firebaseio.com",
-    projectId: "budget-app-56ef8",
-    storageBucket: "budget-app-56ef8.appspot.com",
-    messagingSenderId: "124035025773",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: "1:124035025773:web:8796145cfede8734bb6864",
     measurementId: "G-EN97G2T2YR"
   };
 
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref().set({
-    name: 'Dan'
-});
+const database = firebase.database();
+
+export { firebase, database as default };
